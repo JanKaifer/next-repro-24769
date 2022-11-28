7c20aa070ce2f25ad63875b424bbc169474e1774
Jan Kaifer <jan@kaifer.cz>
2022-11-28 16:13:41 +0100

    added repro

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
   reactStrictMode: true,
+
+  redirects: () => [
+    {
+      "source": "/database/:serie/tk-sm-r/:path*",
+      "destination": "/database/:serie/SM-Kit-du-dresseur-\\(Raichu-d'Alola\\)/:path*", "permanent": true,
+    }
+  ]
 }
